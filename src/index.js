731124b6cb75ff0744867520c0cf1155f2ddb2ff
hakatashi <hakatasiloving@gmail.com>
2015-03-18 11:44:08 +0900

    Draw bars

--- src/index.js
+++ src/index.js
@@ -1,20 +1,32 @@
 var gui = require('nw.gui');
+var pingHistory = [];
+var pingSentTimeHistory = [];
+var bars = [];
+
+// ES6 Math.log10 polyfill
+Math.log10 = Math.log10 || function (x) {
+	return Math.log(x) / Math.LN10;
+};
 
 document.addEventListener('DOMContentLoaded', function () {
 	var ping = require('net-ping');
 
 	var pingTimes = 0;
 	var latestPingTime = new Date();
-	var pingHistory = [];
 
 	var target = '8.8.8.8';
-	var refreshRate = 20;
+	var refreshRate = 30;
+	var interval = 1000;
+	var barInterval = 15;
+	var barWidth = 10;
 
 	var session = ping.createSession({
 		networkProtocol: ping.NetworkProtocol.IPv4,
 	});
 
-	setInterval(doPing, 1000);
+	var draw = Snap('#graph');
+
+	setInterval(doPing, interval);
 	setInterval(refresh, 1000 / refreshRate);
 
 	function doPing() {
@@ -26,15 +38,57 @@ document.addEventListener('DOMContentLoaded', function () {
 			} else {
 				pingHistory[pingSession] = received - sent;
 			}
-			console.log(pingHistory[pingSession]);
 		});
 
 		pingTimes++;
-		latestPingTime = new Date();
+		latestPingTime = pingSentTimeHistory[pingSession] = new Date();
 	}
 
 	function refresh() {
+		var now = new Date();
+		var totalTime = pingTimes * interval + (now - latestPingTime);
+		var scrollLeft = totalTime / interval * barInterval;
+		var screenEl = document.getElementById('graph');
+		var barsEl = screenEl.querySelector('g#bars');
+		var screenWidth = screenEl.clientWidth;
+		var displayingBarNumber = Math.max(0, Math.floor((scrollLeft - screenWidth) / barInterval));
 
+		bars.forEach(function (bar, index) {
+			if (bar && index < displayingBarNumber) {
+				bars[index].remove();
+				delete bars[index];
+			}
+		});
+
+		for (var i = displayingBarNumber; i < pingTimes; i++) {
+			var id = 'bar-' + i;
+			var x = screenWidth - scrollLeft + i * barInterval;
+
+			if (typeof pingHistory[i] === 'undefined') {
+				updateBar(now - pingSentTimeHistory[i]);
+			} else if (typeof pingHistory[i] === 'string') {
+				updateBar(10000);
+			} else {
+				updateBar(pingHistory[i]);
+			}
+
+			function updateBar(ms) {
+				var percentage = Math.log10(ms) / 4 * 100;
+				var attr = {
+					x: x,
+					y: (100 - percentage) + '%',
+					width: barWidth,
+					height: percentage + '%',
+					fill: 'black',
+				};
+
+				if (!bars[i]) {
+					bars[i] = draw.rect(attr.x, attr.y, attr.width, attr.height);
+				} else {
+					bars[i].attr(attr);
+				}
+			}
+		}
 	}
 });
 
